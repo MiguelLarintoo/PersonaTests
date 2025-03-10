09ee91f664f111cee1915423ff4d8fa86bdaad79
= <=>
2025-03-10 12:04:15 +0100

    Añado el archivo de los test unitarios d Junit5

---
+++ src/tests/PersonaTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTests {
+
+	@Test
+	void testPersona() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPersonaStringIntChar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPersonaStringIntCharDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEdad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSexo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPeso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAltura() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalcularIMC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEsMayorDeEdad() {
+		fail("Not yet implemented");
+	}
+
+}
