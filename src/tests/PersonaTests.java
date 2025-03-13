5d5415a11032d692461ffdc52434937d80a7d9ed
MiguelLarintoo <mlarintogarcia@gmail.com>
2025-03-13 09:12:47 +0100

    Añado cambios en el src

--- src/tests/PersonaTests.java
+++ src/tests/PersonaTests.java
@@ -17,76 +17,69 @@ class PersonaTests {
 	     assertTrue(persona.toString().contains("Edad: 0"));
 	 }
 	 @Test
-	    void testConstructorConParametrosBasicos() {
+	 public void testConstructorConParametrosBasicos() {
 	        Persona persona = new Persona("Juan", 25, 'H');
 	        assertTrue(persona.toString().contains("Nombre: Juan"));
 	        assertTrue(persona.toString().contains("Edad: 25"));
 	        assertTrue(persona.toString().contains("Sexo: hombre"));
 	    }
 
-	    @Test
-	    void testConstructorConTodosLosParametros() {
+	 @Test
+	 public void testConstructorConTodosLosParametros() {
 	        Persona persona = new Persona("Ana", 30, 'M', 60.5, 1.65);
 	        assertTrue(persona.toString().contains("Nombre: Ana"));
 	        assertTrue(persona.toString().contains("Edad: 30"));
 	        assertTrue(persona.toString().contains("Sexo: mujer"));
 	    }
-	@Test
-	void testPersonaStringIntChar() {
-	}
-
-	@Test
-	void testPersonaStringIntCharDoubleDouble() {
-	}
 
     @Test
-    void testSetNombre() {
+    public void testSetNombre() {
         Persona persona = new Persona();
         persona.setNombre("Julia");
         assertTrue(persona.toString().contains("Nombre: Julia"));
     }
 
     @Test
-    void testSetEdad() {
+    public void testSetEdad() {
         Persona persona = new Persona();
         persona.setEdad(18);
         assertTrue(persona.toString().contains("Edad: 18"));
     }
 
     @Test
-    void testSetSexo() {
+    public void testSetSexo() {
         Persona persona = new Persona();
         persona.setSexo('M');
         assertTrue(persona.toString().contains("Sexo: mujer"));
     }
 
     @Test
-    void testSetPeso() {
+    public void testSetPeso() {
         Persona persona = new Persona();
         persona.setPeso(65);
         assertTrue(persona.toString().contains("Peso: 65.0 kg"));
     }
 
     @Test
-    void testSetAltura() {
+    public void testSetAltura() {
         Persona persona = new Persona();
         persona.setAltura(1.80);
         assertTrue(persona.toString().contains("Altura: 1.8 metros"));
     }
 
 	@Test
-	void testCalcularIMC() {
+	public void testCalcularIMC() {
 	}
 
 
     @Test
-    void testEsMayorDeEdadTrue() {
+    public void testEsMayorDeEdadTrue() {
         Persona persona = new Persona("Pedro", 20, 'H');
         assertTrue(persona.esMayorDeEdad());
     }
 
     @Test
-    void testEsMayorDeEdadFalse() {
+    public void testEsMayorDeEdadFalse() {
         Persona persona = new Persona("Laura", 16, 'M');
         assertFalse(persona.esMayorDeEdad());
     }
